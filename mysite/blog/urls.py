ea0f610654bee6b305db15b0472f37f803cc4451
seiichi120 <seiichi.120@gmail.com>
2019-02-02 15:48:13 +0900

    url

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('form/', views.form, name='form'),
     path('index/', views.index, name='index'),
-    path('pay/', views.pay, name='pay'),
+    path('form/pay/', views.pay, name='pay'),
 ]
